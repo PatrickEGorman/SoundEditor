76c1a914dc3aa59beafb209fadde10dc5b104325
Patrick Gorman <gormanpe@vcu.edu>
2015-01-26 14:44:26 -0500

    Testing Uploads

--- app/config.py
+++ app/config.py
@@ -5,12 +5,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = "postgres://patrickgorman:soundeditor@localhost:5432/patrickgorman"
+    UPLOAD_FOLDER = '/Users/patrickgorman/documents/uploads'
+    SECRET_KEY = "Sound_editor"
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SECRET_KEY = "Sound_editor"
-UPLOAD_FOLDER = '/Users/patrickgorman/documents/uploads'
+    UPLOAD_FOLDER = os.environ['TRANSLOADIT_URL']
+    SECRET_KEY = os.environ['SECRET_KEY']
+
 ALLOWED_EXTENSIONS = {'mp3', 'wav'}
 app.config['STATIC_FOLDER'] = 'static'
-
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key=SECRET_KEY
+app.secret_key = SECRET_KEY
