47dc91b50a894022f225e7c24323621bb0c298af
Patrick Gorman <gormanpe@vcu.edu>
2015-01-29 20:22:53 -0500

    changes for deployment issues

--- app/models.py
+++ app/models.py
@@ -11,7 +11,6 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String)
     email = db.Column(db.String)
     name = db.Column(db.String)
-    user_directory = db.Column(db.String, unique=True)
     projects = db.relationship('Project', backref='user', lazy='dynamic')
 
     def __init__(self, username, password, email, name):
@@ -19,13 +18,6 @@ class User(db.Model, UserMixin):
         self.password_hash = generate_password_hash(password)
         self.email = email
         self.name = name
-        self.user_directory = self.create_directory()
-
-    def create_directory(self):
-        directory = app.config["UPLOAD_FOLDER"]+self.username
-        if not os.path.exists(directory):
-            os.mkdir(directory)
-        return directory
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
--- app/projects/models.py
+++ app/projects/models.py
@@ -11,18 +11,10 @@ class Project(db.Model):
     project_name = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     sounds = db.relationship("Sound", backref="project", lazy="dynamic")
-    project_directory = db.Column(db.String, unique=True)
 
     def __init__(self, project_name):
         self.project_name = project_name
         self.user_id = g.user.id
-        self.project_directory = self.create_directory()
-
-    def create_directory(self):
-        directory = g.user.user_directory+"/"+self.project_name
-        if not os.path.exists(directory):
-            os.mkdir(directory)
-        return directory
 
     def delete_directory(self):
         shutil.rmtree(self.project_directory)
